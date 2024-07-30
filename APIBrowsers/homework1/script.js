379bb58ce60edf2626a8f4f60f917325d785e82b
Dmitrii <aleksdima@mail.ru>
2024-07-30 13:37:55 +0300

    Homework 1 APIBrowsers completed

--- APIBrowsers/homework1/script.js
+++ APIBrowsers/homework1/script.js
@@ -1,38 +1,98 @@
-const tableData = [
+let tableData = [
     {
-        className: 'Воркаут',
-        time: '10:30',
-        maxPeople: 20,
-        currentPeople: 4
+        "className": "Воркаут",
+        "time": "10:30",
+        "maxPeople": 20,
+        "currentPeople": 4
     },
     {
-        className: 'Бокс',
-        time: '19:30',
-        maxPeople: 8,
-        currentPeople: 5
+        "className": "Бокс",
+        "time": "19:30",
+        "maxPeople": 8,
+        "currentPeople": 0
     },
     {
-        className: 'Cycle',
-        time: '18:30',
-        maxPeople: 24,
-        currentPeople: 12
+        "className": "Cycle",
+        "time": "18:30",
+        "maxPeople": 24,
+        "currentPeople": 12
     },
     {
-        className: 'Плавание',
-        time: '13:40',
-        maxPeople: 12,
-        currentPeople: 7
+        "className": "Плавание",
+        "time": "13:40",
+        "maxPeople": 12,
+        "currentPeople": 12
     }
-];
+]
 
 const tableBody = document.querySelector('tbody');
-tableData.forEach((element, index) => {
-    tableBody.insertAdjacentHTML('beforeend', ` <tr>
+if (localStorage.getItem('classes') !== null) {
+    tableBody.innerHTML = localStorage.getItem('classes');
+}
+else {
+    tableData.forEach((element, index) => {
+        tableBody.insertAdjacentHTML('beforeend', ` <tr>
         <th scope="row">${index + 1}</th>
         <td>${element.className}</td>
         <td>${element.time}</td>
-        <td>${element.maxPeople}</td>
+        <td class="maxPeople">${element.maxPeople}</td>
         <td class="addPeople">${element.currentPeople} </td>
-        <td><button type="button" class="btn btn-success btn-sm">Записаться</button></td>
+        <td><button type="button" class="btn btn-success btn-sm entry">Записаться</button></td>
+        <td><button type="button" class="btn btn-danger btn-sm cancel">Отменить запись</button></td>
     </tr>`);
-});
+    });
+}
+
+const entryButton = document.querySelectorAll('.entry');
+const cancelButton = document.querySelectorAll('.cancel');
+entryButton.forEach(button => {
+    const remBtn = button.parentElement.nextElementSibling.firstChild;
+    if (Number(button.parentNode.parentNode.querySelector('.addPeople').textContent) === 0) {
+        remBtn.classList.add("disabled");
+    }
+    button.addEventListener('click', (e) => {
+        currentPeople = Number(e.target.parentNode.parentNode.querySelector('.addPeople').textContent);
+        const maxPeople = Number(e.target.parentNode.parentNode.querySelector('.maxPeople').textContent);
+
+        if (currentPeople < maxPeople) {
+            if (currentPeople === maxPeople - 1) {
+                e.target.parentNode.parentNode.querySelector('.addPeople').textContent = currentPeople + 1;
+                button.classList.add("disabled")
+            }
+            else {
+                e.target.parentNode.parentNode.querySelector('.addPeople').textContent = currentPeople + 1;
+            }
+            localStorage.setItem('classes', tableBody.innerHTML)
+        }
+        if (currentPeople === 0) {
+            remBtn.classList.remove("disabled")
+            localStorage.setItem('classes', tableBody.innerHTML)
+        }
+    })
+
+})
+cancelButton.forEach(button => {
+    const maxPeople = Number(button.parentNode.parentNode.querySelector('.maxPeople').textContent);
+    const addBtn = button.parentElement.previousElementSibling.firstChild;
+
+    if (Number(button.parentNode.parentNode.querySelector('.addPeople').textContent) === maxPeople) {
+        addBtn.classList.add("disabled");
+    }
+    button.addEventListener('click', (e) => {
+        let currentPeople = Number(e.target.parentNode.parentNode.querySelector('.addPeople').textContent);
+        if (currentPeople < maxPeople && currentPeople > 1) {
+            e.target.parentNode.parentNode.querySelector('.addPeople').textContent = currentPeople - 1;
+            localStorage.setItem('classes', tableBody.innerHTML)
+        }
+        else if (currentPeople === maxPeople) {
+            e.target.parentNode.parentNode.querySelector('.addPeople').textContent = currentPeople - 1;
+            addBtn.classList.remove("disabled")
+            localStorage.setItem('classes', tableBody.innerHTML)
+        }
+        else if (currentPeople === 1) {
+            e.target.parentNode.parentNode.querySelector('.addPeople').textContent = currentPeople - 1;
+            button.classList.add("disabled")
+            localStorage.setItem('classes', tableBody.innerHTML)
+        }
+    })
+})
