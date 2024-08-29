7e858c3d6d42bd61445e6db44cdb9c20013dc4b9
Dmitrii <aleksdima@mail.ru>
2024-08-29 16:47:04 +0300

    homework 3 before completed

--- AdvancedJS/homework3/reviews.js
+++ AdvancedJS/homework3/reviews.js
@@ -1,19 +1,52 @@
 const productName = document.querySelector(".product_name");
 const noReviews = document.createElement("p");
 noReviews.innerHTML = `<p style='color: red;'>Отзывов пока нет!</p`;
-if (localStorage.length != 0) {
-    for (let index = 0; index < localStorage.length; index++) {
-        const element = localStorage.key(index);
-        console.log(element);
-        productName.insertAdjacentHTML("beforeend", `<li>${localStorage.key(index)}<ul class="product_review">
-                ${localStorage.getItem(localStorage.key(index))}
+
+function checkEmpty() {
+    if (localStorage.length != 0) {
+        for (let index = 0; index < localStorage.length; index++) {
+            const element = localStorage.key(index);
+            productName.insertAdjacentHTML("beforeend", `<li class="top_li top_li2">${localStorage.key(index)}<ul class="product_review hidden">${localStorage.getItem(localStorage.key(index))}
             </ul></li>`)
+        }
+    } else {
+        productName.prepend(noReviews);
     }
-} else {
-    productName.prepend(noReviews);
 }
+checkEmpty();
 
 const toWriteReview = document.querySelector('.to_write_review');
 toWriteReview.addEventListener('click', () => {
     window.location.href = './index.html';
+})
+
+function event() {
+
+}
+
+const topLi = document.querySelectorAll(".top_li");
+topLi.forEach(element => {
+    element.addEventListener('click', (e) => {
+        if (e.target.classList.contains("top_li")) {
+            e.target.closest(".top_li").firstElementChild.classList.toggle("hidden");
+            element.closest("li").classList.toggle("top_li2");
+
+        }
+        if (e.target.classList.contains("delete_button")) {
+            // var tag; //Элемент, содержимое которого без вложенных тегов нужно получить 
+            const key = e.target.parentNode.parentNode.childNodes[0].textContent;
+            e.target.previousElementSibling.remove();
+            const productReview = document.querySelector('.product_review');
+            console.log(e.target.parentNode);
+            e.target.remove();
+            localStorage.setItem(key, productReview.innerHTML);
+            let d = localStorage.getItem(key);
+            d = d.replaceAll('\n', '').trim();
+            console.log(d.length);
+            if (d === '') {
+                localStorage.removeItem(key);
+            }
+            checkEmpty()
+        }
+    });
 })
--- AdvancedJS/homework3/script.js
+++ AdvancedJS/homework3/script.js
@@ -17,31 +17,47 @@
 // if (localStorage.getItem("review") !== null) {
 //     textcontainer.innerHTML = localStorage.getItem("review");
 // }
+const reviewsDiv = document.querySelector(".review");
+const message = document.createElement("p");
 const p = document.createElement("p");
 p.innerHTML = `<p style='color: red;'>Название продукта не может быть пустым!</p`;
 const textcontainer = document.createElement("div")
 
 button.addEventListener("click", () => {
     const productName = document.querySelector(".product_name__input");
+    //reviewsDiv.removeChild(message);
+    if (reviewsDiv.contains(message)) {
+        reviewsDiv.contains(message);
+    }
     if (productName.parentNode.contains(p)) {
         productName.parentNode.removeChild(p);
     }
     if (productName.value == "") {
         productName.parentNode.prepend(p);
     }
+    else if (input.value.length == 0) {
+        message.innerHTML = `<p style='color: red;'>Отзыв не может быть пустым!</p`;
+        reviewsDiv.append(message);
+    }
     else {
-        if (input.value.length > 500)
+        if (input.value.length > 5000)
             throw new Error("Отзыв слишком длинный");
+
         if (localStorage.getItem(`${productName.value}`) != null) {
             textcontainer.innerHTML = `${localStorage.getItem(`${productName.value}`)}`
         }
         else {
             textcontainer.innerHTML = ''
         }
-        textcontainer.insertAdjacentHTML("beforeend", `<li>${input.value}</li>`);
+        textcontainer.insertAdjacentHTML("beforeend", `<li class="down_li">${input.value}</li><button class="delete_button" type="button">Удалить</button>`);
         input.value = "";
         localStorage.setItem(`${productName.value}`, textcontainer.innerHTML);
         productName.value = "";
+        message.innerHTML = `<p style='color: red;'>Отзыв успешно размещен!</p`;
+        reviewsDiv.append(message);
+        setTimeout(() => {
+            reviewsDiv.removeChild(message);
+        }, 2000);
     }
 })
 const buttonToReviews = document.querySelector(".to_reviews");
